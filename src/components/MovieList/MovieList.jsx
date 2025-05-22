a217f306af0ba362cf1c6069b925c908cae43973
novikovanat <72397136+novikovanat@users.noreply.github.com>
2025-05-22 19:40:17 +0200

    imports changed

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,4 @@
 import toggleFavorite from "../../js/toggleFavorite";
-import MovieCard from "../MovieCard/MovieCard";
 
 export default function MovieList({ moviesArray, onClick }) {
   function handleClick(event) {
