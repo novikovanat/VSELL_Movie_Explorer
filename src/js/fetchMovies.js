7c9b571332b91378a9a3d67ff11227380720bdab
novikovanat <72397136+novikovanat@users.noreply.github.com>
2025-05-22 19:46:46 +0200

    address changed

--- src/js/fetchMovies.js
+++ src/js/fetchMovies.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://www.omdbapi.com/?apikey=c2b35b46&";
+axios.defaults.baseURL = "https://www.omdbapi.com/?apikey=c2b35b46&";
 
 export default async function fetchMovies(searchTerm, page) {
   const result = await axios.get("/", {
