2d66a9aaaf30e15ca5182f6a25526a37c784ea03
novikovanat <72397136+novikovanat@users.noreply.github.com>
2025-05-24 13:45:51 +0200

    cache added

--- src/hooks/useCachedFetch.js
+++ src/hooks/useCachedFetch.js
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import fetchMovies from "../js/fetchMovies";
 
 const useCashedFetch = (query, page) => {
+  const cashe = useRef({});
   const [response, setResponse] = useState({
     Search: [],
     totalResults: 0,
@@ -18,16 +19,24 @@ const useCashedFetch = (query, page) => {
     search(query, page);
   }, [query, page]);
 
-  const search = async (query, page) => {
+  const search = async (query, page = 1) => {
     try {
       setLoading(true);
       setError("");
-      const apiResponse = await fetchMovies(query, page);
+      console.log(page);
+      console.log(cashe.current[query + page]);
 
-      if (apiResponse.Response == "False") {
+      if (cashe.current[query + page]) {
+        setResponse(cashe.current[query + page].apiResponse);
+        return;
+      }
+      const apiResponse = await fetchMovies(query, page);
+      if (apiResponse.Response === "False") {
         setError(apiResponse.Error);
         return;
       }
+      cashe.current[query + page] = { apiResponse, page };
+      console.log("fetching from API");
       setResponse(apiResponse);
     } catch (error) {
       setError(error.message);
